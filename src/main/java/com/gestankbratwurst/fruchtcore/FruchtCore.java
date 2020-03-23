6e44181957bde80e05b17f1144c264ba72b271d4
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 17:57:00 +0100

    Initial commit

--- src/main/java/com/gestankbratwurst/fruchtcore/FruchtCore.java
+++ src/main/java/com/gestankbratwurst/fruchtcore/FruchtCore.java
@@ -4,25 +4,27 @@ import co.aikar.commands.PaperCommandManager;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.gestankbratwurst.fruchtcore.io.IOManager;
+import com.gestankbratwurst.fruchtcore.io.JsonFile;
 import com.gestankbratwurst.fruchtcore.tasks.TaskManager;
 import com.gestankbratwurst.fruchtcore.util.Msg;
 import com.gestankbratwurst.fruchtcore.util.UtilModule;
-import com.gestankbratwurst.fruchtcore.util.holograms.MovingHologram;
-import com.gestankbratwurst.fruchtcore.util.holograms.impl.HologramManager;
-import com.gestankbratwurst.fruchtcore.util.holograms.impl.HologramTextLine;
 import com.gestankbratwurst.fruchtcore.util.items.display.ItemDisplayCompiler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.Getter;
 import lombok.Setter;
 import net.crytec.inventoryapi.InventoryAPI;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 
 public final class FruchtCore extends JavaPlugin {
+
+  @Getter
+  private static FruchtCore instance;
 
   @Getter
   private IOManager ioManager;
@@ -40,6 +42,7 @@ public final class FruchtCore extends JavaPlugin {
 
   @Override
   public void onEnable() {
+    instance = this;
     new InventoryAPI(this);
     Msg.init(this);
     this.utilModule = new UtilModule();
@@ -51,38 +54,6 @@ public final class FruchtCore extends JavaPlugin {
     utilModule.enable(this);
     ioManager.loadMappings();
     ioManager.loadAllFiles();
-
-    Bukkit.getPluginManager().registerEvents(new Listener() {
-
-      private final HologramManager hologramManager = utilModule.getHologramManager();
-      private final int ticksAlive = 60;
-      private final Vector direction = new Vector(0D, 0.15D, 0D);
-
-      private final String[] lines = new String[]{"§aLine", "§cLine", "§eLine"};
-
-      @EventHandler
-      public void handleSneak(BlockBreakEvent event) {
-
-        Location loc = event.getBlock().getLocation().add(0.5, 0.5, 0.5);
-        MovingHologram moving = hologramManager.createMovingHologram(loc, direction, ticksAlive);
-        moving.getHologram().appendTextLine("");
-
-        taskManager.runFixedTimesBukkitSync(new Runnable() {
-          private int index = 0;
-
-          @Override
-          public void run() {
-            ((HologramTextLine) moving.getHologram().getHologramLine(0)).update(lines[index++]);
-            if (index == lines.length) {
-              index = 0;
-            }
-          }
-        }, 0L, 5L, 11);
-
-      }
-
-    }, this);
-
   }
 
   @Override
